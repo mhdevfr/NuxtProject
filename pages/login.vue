4b90e8d234967ff41861d6c38fa7b4863de982e5
milan <milan.herbe@gmail.com>
2024-03-10 16:05:32 +0100

    login Oauth Github work

--- pages/login.vue
+++ pages/login.vue
@@ -2,17 +2,12 @@
     <div class="relative inset-0 w-full bg-white bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"> 
         <navbar class="absolute l-0"/>
         <div class="min-w-full min-h-screen  flex items-center justify-center ">
-            <div class="min-h-screen min-w-full flex items-center justify-center">
+            <div class="h-1/2 min-w-full flex items-center justify-center">
                 <form 
                     class="flex flex-col  w-3/12 justify-center bg-black items-center border rounded-xl">
-                    <h1 class="text-3xl mt-2 text-white">Se connecter</h1>
-                    <input type="email" v-model="email" placeholder="your email"
-                        class="h-8 w-3/4 text-center my-4 mt-4">
-                    <input type="password" v-model="password" placeholder="your password"
-                        class="h-8 w-3/4 text-center my-4">
-                    <button class="text-center rounded h-8 min-w-48 mb-8 bg-green-400" @click="signUp()">
-                        {{ isSignUp ? 'Connexion' : 'Inscription' }}
-                    </button>
+                    <h1 class="text-3xl mt-2 text-white">Se connecter avec Github</h1>
+                    <img src="../images/icons8-github-90.png">
+                    <button class="text-black bg-green-300 h-8 my-16 rounded-xl w-1/2" @click="signInWithGithub()">Connect with github</button>
                 </form>
             </div>
         </div>
@@ -20,39 +15,17 @@
 </template>
 
 <script setup lang="ts">
-
-
-const client = useSupabaseClient();
-const isSignUp = ref(false)
-const email = ref('')
-const password = ref ('')
-
-const signUp = async () => {
-    const { user, error } = await client.auth.signUp({
-        email: email.value,
-        password: password.value,
-    })
-    console.log('user', user)
-    console.log('error', error)
-    console.log('register')
+async function signInWithGithub() {
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'github',
+  })
 }
 
-const login = async () => {
-    const { user, error } = await client.auth.signIn({
-        email: email.value,
-        password: password.value,
-    })
-    console.log('user', user)
-    console.log('error', error)
-    console.log('login')
-}
+import { createClient } from '@supabase/supabase-js'
 
-const user = useSupabaseUser()
-onMounted(() => {
-    watchEffect(() => {
-        if(user.value){
-            navigateTo('./bio')
-        }
-    })
-})
+const supabaseUrl = 'https://moaulmnomptksngvnboj.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vYXVsbW5vbXB0a3NuZ3ZuYm9qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk5MTc1NjUsImV4cCI6MjAyNTQ5MzU2NX0.dOeKSg3blO0MVbnCH6cvfhQ4UFJ1C5S88lQkd01S8F4"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
 </script>
