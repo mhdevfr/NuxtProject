a7d4f82d0b586b8370da5e9b3183dddd3ce0bd76
milan <milan.herbe@gmail.com>
2024-03-14 17:32:56 +0100

    Done

--- pages/contact.vue
+++ pages/contact.vue
@@ -7,9 +7,9 @@
                 <input class="bg-transparent border-b-2 w-3/12  border-slate-950 mt-8 text-black" type="text" placeholder="Votre prénom">
                 <div class="flex">
                     <input class="bg-transparent  border-slate-950 border-b-2 w-3/12 mt-4 text-black" type="text" placeholder="Le sujet">
-                    <img src="../images/icons8-github-50-2.png" class="w-10 h-10 mt-10 ml-96">
-                    <img src="../images/icons8-linkedin-100.png" class="w-10 h-10 mt-10 ml-24">
-                    <img src="../images/icons8-courrier-60.png" class="w-10 h-10 mt-10 ml-24">
+                    <img src="../images/icons8-courrier-60.png" class="w-10 h-10 mt-10 ml-96">
+                    <img src="../images/icons8-linkedin-50 (1).png" class="w-10 h-10 mt-10 ml-24">
+                    <img src="../images/icons8-github-50.png" class="w-10 h-10 mt-10 ml-24">
                 </div>
                 <input class="bg-transparent  border-slate-950 border-2 min-h-64 my-8 w-3/4 text-black" type="text" placeholder="le contenu de votre message">
                 <input type="submit" class="submit bg-black text-white w-32 h-8 hover:bg-green-400 transition:2000">
