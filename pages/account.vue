3c8778120c8ec04ec6e3402966a5a54930464e91
milan <milan.herbe@gmail.com>
2024-03-18 19:22:48 +0100

    Account

--- pages/account.vue
+++ pages/account.vue
@@ -1,8 +1,7 @@
 <template>
       <div class="relative inset-0 max-w-full flex items-center flex-col justify-end min-h-screen bg-yellow-300"> 
-              <div class="w-5/6 ml-10 h-80 bg-black mb-5 rounded-lg text-white">
-                <h1 class="m-10 text-3xl">Your NFT Collections</h1>
-              </div>
+        <navbar class="absolute left-0"></navbar>
+              <nftCollection/>
               <div class="flex justify-center items-center w-full ml-10 mb-5">
                 <div>
                   <settings/>
